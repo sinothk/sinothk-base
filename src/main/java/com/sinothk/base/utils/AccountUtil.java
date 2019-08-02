26bd840db6b0b3299957130f75d8445f130be832
LiangYT <381518188@qq.com>
2019-08-02 17:13:47 +0800

    处理IDUtil

--- src/main/java/com/sinothk/base/utils/AccountUtil.java
+++ src/main/java/com/sinothk/base/utils/AccountUtil.java
@@ -6,15 +6,23 @@ public class AccountUtil {
 
     private static Set<Long> accountSet = new HashSet<>();
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        Set<Long> initSet = new HashSet<>();
+//        initSet.add(55555L);
+//        AccountUtil.init(initSet);
+//
+//        System.out.println("产出Id = " + AccountUtil.create());
+//
+//        Set<Long> newSet = AccountUtil.getAccountSet();
+//        System.out.println("Id Size = " + newSet.size());
+//    }
 
-        AccountUtil.init(null);
-
-        System.out.println("产出Id = " + AccountUtil.create());
+    public static Set<Long> getAccountSet() {
+        return accountSet;
     }
 
     public static long create() {
-
         List<Long> list = new ArrayList<>(accountSet);
         Collections.sort(list);
 
@@ -25,12 +33,12 @@ public class AccountUtil {
     }
 
     public static void init(Set<Long> initSet) {
+        accountSet.clear();
+
         if (initSet == null || initSet.size() == 0) {
             initSet = new HashSet<>();
             initSet.add(10000L);
         }
-
-        accountSet.clear();
         accountSet.addAll(initSet);
     }
 }

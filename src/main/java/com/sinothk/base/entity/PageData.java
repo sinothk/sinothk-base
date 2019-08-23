3475bca521ce818d6ef45e77550bce6fe0e4a149
LiangYT <381518188@qq.com>
2019-08-23 11:19:53 +0800

    处理JWTUtil

--- src/main/java/com/sinothk/base/entity/PageData.java
+++ src/main/java/com/sinothk/base/entity/PageData.java
@@ -15,7 +15,40 @@ public class PageData<T> {
     //每页的数量
     private int pageSize;
 
+    //是否有下一页
+    private boolean hasMore;
+
     private T data;
-    
-    
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public boolean isHasMore() {
+        return hasMore;
+    }
+
+    public void setHasMore(boolean hasMore) {
+        this.hasMore = hasMore;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
